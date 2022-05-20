2b9644004844c7d2dc13224c0b9f6eadae64c432
Adrián Escat <adrian.escat@gmail.com>
2022-05-19 21:59:44 -0300

    Play sound only when boosting

--- rocket-boost-game/Assets/Scripts/Movement.cs
+++ rocket-boost-game/Assets/Scripts/Movement.cs
@@ -7,10 +7,12 @@ public class Movement : MonoBehaviour
   [SerializeField] float mainThrust = 1000f;
   [SerializeField] float rotationThrust = 100f;
   Rigidbody rb;
+  AudioSource audioSource;
   // Start is called before the first frame update
   void Start()
   {
     rb = GetComponent<Rigidbody>();
+    audioSource = GetComponent<AudioSource>();
   }
 
   // Update is called once per frame
@@ -25,6 +27,14 @@ public class Movement : MonoBehaviour
     if (Input.GetKey(KeyCode.Space))
     {
       rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+      if (!audioSource.isPlaying)
+      {
+        audioSource.Play();
+      }
+    }
+    else
+    {
+      audioSource.Stop();
     }
   }
 
